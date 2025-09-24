2c35d38349ddbb664d8889d20dbae877d8abaf88
Ryan Fredrickson <rtsfred3@gmail.com>
2025-09-23 20:10:07 -0700

    Test 2

---
+++ scripts/generateHostToAdblockPro.sh
@@ -0,0 +1,41 @@
+clear && clear
+
+DIR="abp"
+
+# FILE=adlist.host.abp.txt
+# HOST_LISTS=("https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" "https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt" "https://raw.githubusercontent.com/ph00lt0/blocklist/master/hosts-blocklist.txt")
+# FILE=$(echo $HOST_LIST | cut -d "/" -f7)
+
+# FILE=frellwits.swedish.abp.txt
+# HOST_LIST="https://raw.githubusercontent.com/lassekongo83/Frellwits-filter-lists/master/Frellwits-Swedish-Hosts-File.txt"
+
+FILE="$1"
+HOST_LIST="$2"
+
+echo $FILE
+echo $HOST_LIST
+
+DEDUPED_FILE="tmp_${FILE}"
+
+if [ ! -d "$DIR" ]; then
+    mkdir $DIR
+fi
+
+echo "" > $DIR/$FILE
+
+echo "[Adblock Plus]
+! Generated at $(date +'%Y-%m-%dT%H:%M:%SZ')
+! 
+! Sources: $HOST_LIST" > $DIR/$DEDUPED_FILE
+
+curl "$HOST_LIST" --silent | sed '/^#/d' | sed -E 's/^[^ #]+[ ]+(.+)$/\1/g' | sort >> $DIR/$FILE
+
+echo "! " >> $DIR/$DEDUPED_FILE
+
+echo "$(cat $DIR/$FILE | sort | sed -E '/^[ 	]+#/d' | sed -E 's/^(.+) #.*$/\1/g' | sed '/^$/d' | sed '/^0\.0\.0\.0$/d' | sort -u)" > $DIR/$FILE
+echo "$(cat $DIR/$FILE | sort | sed -E 's/^(.+)$/||\1^/g')" >> $DIR/$DEDUPED_FILE
+
+mv $DIR/$DEDUPED_FILE $DIR/$FILE
+
+gzip -k -9 -f $DIR/$FILE
+# gzip -k -9 -f $DIR/$DEDUPED_FILE
